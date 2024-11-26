45fa602d6b1ce3605117684c9fec197da35e3cf0
tknr <geishagirlskoume@gmail.com>
2024-11-26 16:51:36 +0900

    init

--- Radiko_Recorder.sh
+++ Radiko_Recorder.sh
@@ -1,2 +1,27 @@
-#!/usr/bin/env sh
-exec "${PYTHON:-python3}" "-m" "radiko_recorder" "$@"
+#!/usr/bin/env bash
+cd $(dirname $0)
+set -e
+PYTHON_COMMAND="python3"
+
+if ! command -v python3 &>/dev/null; then
+    if ! command -v python &>/dev/null; then
+        echo "Error: Python not found, please install python 3.8 or higher and try again"
+        exit 1
+    fi
+fi
+
+if command -v python &>/dev/null; then
+   PYTHON_COMMAND="python"
+fi
+
+echo "Found $PYTHON_COMMAND command"
+
+python_version=$($PYTHON_COMMAND --version 2>&1 | awk '{print $2}')  
+echo "Python version : $python_version"
+
+BASEDIR=$(pwd)
+# shellcheck disable=SC1091
+$PYTHON_COMMAND -m virtualenv --python=python3.12 "$BASEDIR/python312"
+source "$BASEDIR/python312/bin/activate"
+
+exec "${PYTHON_COMMAND}" "-m" "radiko_recorder" "$@"
--- build.sh
+++ build.sh
@@ -1,4 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+cd $(dirname $0)
+set -e
+PYTHON_COMMAND="python3"
+
+if ! command -v python3 &>/dev/null; then
+    if ! command -v python &>/dev/null; then
+        echo "Error: Python not found, please install python 3.8 or higher and try again"
+        exit 1
+    fi
+fi
+
+if command -v python &>/dev/null; then
+   PYTHON_COMMAND="python"
+fi
+
+echo "Found $PYTHON_COMMAND command"
+
+python_version=$($PYTHON_COMMAND --version 2>&1 | awk '{print $2}')  
+echo "Python version : $python_version"
+
+BASEDIR=$(pwd)
+
+# shellcheck disable=SC1091
+pip install virtualenv
+$PYTHON_COMMAND -m virtualenv --python=python3.12 "$BASEDIR/python312"
+source "$BASEDIR/python312/bin/activate"
+
+$PYTHON_COMMAND -m pip install wheel
+$PYTHON_COMMAND -m pip install -r "$BASEDIR/requirements.txt"
+
 
 echo "Build start..."
 
@@ -26,3 +56,4 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Build complete!"
+
